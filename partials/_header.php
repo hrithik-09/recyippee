9268912dea4e1145b8c30620447daead9fd43a10
Hrithik <ranjan.hrithik09@gmail.com>
2023-05-13 15:01:48 +0530

    Home page design done

---
+++ partials/_header.php
@@ -0,0 +1,33 @@
+<?php
+echo'
+<header>
+<nav class="navbar navbar-expand-md fixed-top navbar-dark bg-primary">
+<div class="container-fluid">
+<a class="navbar-brand" href="#" style="font-family: "Roboto", sans-serif;"><strong>Recyippee</strong></a>
+<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+<li class="nav-item">
+<a class="nav-link text-light" href="index.php">Home <span class="sr-only"></span></a>
+</li>
+<li class="nav-item">
+<a class="nav-link text-light" href="add.php">Add Recipe</a>
+</li>
+<li class="nav-item">
+<a class="nav-link text-light" href="about.php">About</a>
+</li>
+
+<li class="nav-item">
+<a class="nav-link text-light" href="contact.php">Contact Us</a>
+        </li>
+        </ul>
+        <button class="btn btn-outline-light ms-2" data-bs-toggle="modal" data-bs-target="#loginModal">Log In</button>
+        <button class="btn btn-outline-light mx-2" data-bs-toggle="modal" data-bs-target="#signupModal">Sign Up</button>;
+        </div>
+        </div>
+        </nav>
+        </header>
+        ';
+        ?>
